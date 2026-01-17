45fd8137362c8cc9f9ec926a1e9723e4185b6d77
Wellerson Ferreira Queiroz de Souza <107362025+Well-Hack@users.noreply.github.com>
2026-01-17 12:40:49 -0300

    blockchaintreino 0.2

---
+++ src/createWallet.js
@@ -0,0 +1,33 @@
+//Importando as dependencias,
+const bip32 = require('bip32')
+const bip39 = require('bip39')
+const bitcoin = require('bitcoinjs-lib')
+
+//definir rede
+//bitcoin - rede principal - mainet
+//testenet - rede de teste
+const network = bitcoin.networks.testnet
+
+//derivação de endereços e carteiras HD (hierarquica determinista)
+const path = `m/49'/1'/0'/0`
+
+//criando o mnemonic para o seed(palavra senha)
+let mnemonic = bip39.generateMnemonic()
+const seed = bip39.mnemonicToSeedSync(mnemonic)
+
+//criando a raiz da carteira HD
+let root = bip32.fromSeed(seed, network)
+
+//Criando uma conta - par pvt-pub keys
+let account = root.derivePath(path)
+let node = account.derive(0).derive(0)
+
+let btcAddress = bitcoin.payments.p2pkh({
+    pubkey: node.publicKey,
+    network: network, 
+}).adress
+
+console.log("Carteira gerada")
+console.log("Endereço: ", btcAddress)
+console.log("Private key: ", node.toWIF());
+console.log("Seed", mnemonic)
